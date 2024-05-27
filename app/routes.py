fcb7dd2c6f0691e931cfbaca9feac7d246d0ef2d
madgic <olejikqq@gmail.com>
2024-05-28 00:14:34 +0300

    add route(/add/) and 2 html page

---
+++ app/routes.py
@@ -0,0 +1,20 @@
+from flask import redirect, render_template, url_for, request
+from app import app
+
+
+
+dp = ['1', '2', '3','4', '5', '6']
+@app.route('/index/')
+@app.route('/')
+def index():
+    return render_template('base.html', tasks=dp)
+
+
+@app.route('/add/', methods=['GET', 'POST'])
+def add_task():
+    if request.method == 'POST':
+        task = request.form['task']
+        dp.append(task)
+        return redirect(url_for('index'))
+    return render_template('add_task.html')
+
